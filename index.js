652bd1ffcbee07c12c26a6a050b0097c45feef7d
Shqdowsky <117082774+Shqdowsky@users.noreply.github.com>
2024-12-02 21:43:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use('/', router);
 
 async function startApp(){
     try{
-        await mongoose.connect(url);
+        await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
         app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
     }catch(e){
         console.log(e);
